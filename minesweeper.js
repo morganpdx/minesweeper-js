6a3dac29a40be40da3f60e013be116faeed0da25
Miranda Fox <miranda.fox@nytimes.com>
2022-08-03 11:12:49 -0700

    WIP Swarm test Maker Week 202222

--- minesweeper.js
+++ minesweeper.js
@@ -1,7 +1,21 @@
-
-
 export function createBoard(boardSize, numberOfMines) {
-  const board = []
-  
-  return board
+    const board = []
+
+    for (let i = 0; i < boardSize; i++) {
+        const row = []
+
+        for (let j = 0; j < boardSize; j++) {
+            const cell = { isBomb: true }
+            row.push(cell)
+        }
+        board.push(row)
+    }
+
+    return board
 }
+
+export function minesweeper() {
+    console.log("minesweeper minesweeper")
+}
+
+minesweeper()
--- test.js
+++ test.js
@@ -1,6 +1,30 @@
-const assert = require('assert')
+import assert from 'assert'
+import { createBoard } from "./minesweeper.js"
 
+describe('create board', () => {
+    it('should return a board with height and width equal to board size', () => {
+        const boardSize = 10
 
-it('should return true', () => {
-  assert.equal(true, false)
+        const board = createBoard(boardSize, 0)
+
+        assert.equal(board.length, boardSize)
+        assert.equal(board[0].length, boardSize)
+
+    })
+
+    it('should create board cells', () => {
+        const board = createBoard(5, 0)
+
+        //assert.deepEqual(, ['isBomb'])
+        assert.deepEqual(Object.keys(board[0][0]), ['isBomb'])
+    })
 })
+
+// var assert = require('assert');
+// describe('Array', function() {
+//     describe('#indexOf()', function() {
+//         it('should return -1 when the value is not present', function() {
+//             assert.equal([1, 2, 3].indexOf(4), -1);
+//         });
+//     });
+// });boardSize()s4//
